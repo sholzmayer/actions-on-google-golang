9b7319712b2a1bba28a62b87954a6aaf44cf139c
Simon Holzmayer <holzmayer@edomo.de>
2017-10-02 11:11:28 +0200

    define context as type

--- model/request.go
+++ model/request.go
@@ -5,18 +5,14 @@ import "time"
 type ApiAiRequest struct {
 	Id        string    `json:"id"`
 	Timestamp time.Time `json:"timestamp"`
-	Result    struct {
+	Result struct {
 		Source           string            `json:"source"`
 		ResolvedQuery    string            `json:"resolvedQuery"`
 		Speech           string            `json:"speech"`
 		Action           string            `json:"action"`
 		ActionIncomplete bool              `json:"actionIncomplete"`
 		Parameters       map[string]string `json:"parameters"`
-		Contexts         []struct {
-			Name       string            `json:"name"`
-			Parameters map[string]string `json:"parameters"`
-			Lifespan   int               `json:"lifespan"`
-		} `json:"contexts"`
+		Contexts         []Context         `json:"contexts"`
 		Metadata struct {
 			IntentID                  string `json:"intentId"`
 			WebhookUsed               string `json:"webhookUsed"`
@@ -24,7 +20,7 @@ type ApiAiRequest struct {
 			IntentName                string `json:"intentName"`
 		} `json:"metadata"`
 		Fulfillment struct {
-			Speech   string `json:"speech"`
+			Speech string `json:"speech"`
 			Messages []struct {
 				Type   int    `json:"type"`
 				Speech string `json:"speech"`
@@ -36,24 +32,24 @@ type ApiAiRequest struct {
 		Code      int    `json:"code"`
 		ErrorType string `json:"errorType"`
 	} `json:"status"`
-	SessionID       string `json:"sessionId"`
+	SessionID string `json:"sessionId"`
 	OriginalRequest struct {
 		Source string `json:"source"`
-		Data   struct {
+		Data struct {
 			Inputs []struct {
 				Arguments []struct {
 					RawText   string `json:"raw_text"`
 					TextValue string `json:"text_value"`
 					Name      string `json:"name"`
 				} `json:"arguments"`
-				Intent    string `json:"intent"`
+				Intent string `json:"intent"`
 				RawInputs []struct {
 					Query     string `json:"query"`
-					InputType string    `json:"input_type"`
+					InputType string `json:"input_type"`
 				} `json:"raw_inputs"`
 			} `json:"inputs"`
 			User struct {
-				UserID  string `json:"user_id"`
+				UserID string `json:"user_id"`
 				Profile struct {
 					DisplayName string `json:"display_name"`
 					GivenName   string `json:"given_name"`
@@ -75,8 +71,14 @@ type ApiAiRequest struct {
 			Conversation struct {
 				ConversationToken string `json:"conversation_token"`
 				ConversationID    string `json:"conversation_id"`
-				Type              string    `json:"type"`
+				Type              string `json:"type"`
 			} `json:"conversation"`
 		} `json:"data"`
 	} `json:"originalRequest"`
+}
+
+type Context struct {
+	Name       string            `json:"name"`
+	Parameters map[string]string `json:"parameters"`
+	Lifespan   int               `json:"lifespan"`
 }
