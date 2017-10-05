e55424e31c9eb61cc24fbc16ec8ff79b9a64dd06
Simon Holzmayer <holzmayer@edomo.de>
2017-10-05 18:24:47 +0200

    add expectedInput property

--- model/response.go
+++ model/response.go
@@ -1,21 +1,22 @@
 package model
 
 type ServiceResponse struct {
-	Speech string `json:"speech"`
+	Speech      string `json:"speech"`
 	DisplayText string `json:"displayText"`
 	Data struct {
 		Google struct {
 			ExpectUserResponse bool `json:"expect_user_response"`
-			IsSsml bool `json:"is_ssml"`
+			IsSsml             bool `json:"is_ssml"`
 			PermissionsRequest struct {
-				OptContext string `json:"opt_context"`
+				OptContext  string   `json:"opt_context"`
 				Permissions []string `json:"permissions"`
 			} `json:"permissions_request"`
+			ExpectedInput []ExpectedInput `json:"expectedInput, omitempty"`
 		} `json:"google"`
 	} `json:"data"`
 	ContextOut []struct {
-		Name string `json:"name"`
-		Lifespan int `json:"lifespan"`
+		Name     string `json:"name"`
+		Lifespan int    `json:"lifespan"`
 		Parameters struct {
 			City string `json:"city"`
 		} `json:"parameters"`
@@ -23,4 +24,29 @@ type ServiceResponse struct {
 
 	//TODO
 	//add contextOut
+}
+
+type ExpectedInput struct {
+	InputPrompt                      `json:"inputPrompt"`
+	PossibleIntents []PossibleIntent `json:"possibleIntents"`
+}
+
+type InputPrompt struct {
+	InitialPrompts []InitialPrompt `json:"initialPrompts"`
+	NoInputPrompts []interface{}   `json:"noInputPrompts"`
+}
+
+type InitialPrompt struct {
+	TextToSpeech string `json:"textToSpeech"`
+}
+
+type PossibleIntent struct {
+	Intent string  `json:"intent"`
+	InputValueData `json:"inputValueData"`
+}
+
+type InputValueData struct {
+	Type        string   `json:"@type"`
+	OptContext  string   `json:"optContext"`
+	Permissions []string `json:"permissions"`
 }
